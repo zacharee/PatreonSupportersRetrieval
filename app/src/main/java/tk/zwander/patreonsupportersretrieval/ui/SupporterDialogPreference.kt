93593497a3950594bd5c33ed4e090a5ee29aad8b
zacharee <zachary.wander@gmail.com>
2021-02-06 21:10:33 -0500

    Add SupporterDialogPreference

---
+++ app/src/main/java/tk/zwander/patreonsupportersretrieval/ui/SupporterDialogPreference.kt
@@ -0,0 +1,19 @@
+package tk.zwander.patreonsupportersretrieval.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.preference.Preference
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import tk.zwander.patreonsupportersretrieval.R
+import tk.zwander.patreonsupportersretrieval.view.SupporterView
+
+class SupporterDialogPreference(context: Context, attrs: AttributeSet) : Preference(context, attrs) {
+    override fun onClick() {
+        super.onClick()
+
+        MaterialAlertDialogBuilder(context)
+                .setTitle(R.string.supporters)
+                .setView(SupporterView(context))
+                .show()
+    }
+}
