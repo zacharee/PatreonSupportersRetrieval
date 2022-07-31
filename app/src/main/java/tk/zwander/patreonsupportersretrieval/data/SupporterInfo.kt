8436bbdbdcb65956d766894f91e9763f53b2812f
Zachary Wander <zachary.wander@gmail.com>
2022-07-31 14:12:21 -0400

    Make SupporterInfo.kt Parcelable

--- app/src/main/java/tk/zwander/patreonsupportersretrieval/data/SupporterInfo.kt
+++ app/src/main/java/tk/zwander/patreonsupportersretrieval/data/SupporterInfo.kt
@@ -1,6 +1,10 @@
 package tk.zwander.patreonsupportersretrieval.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class SupporterInfo(
     val name: String,
     val link: String
-)
+) : Parcelable
